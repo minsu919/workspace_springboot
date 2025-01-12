7ab74eca865bdd5439b504931f9a3a98328653ee
minsu <minsur0919@gmail.com>
2025-01-12 12:34:17 +0900

    oswin

--- demo/src/main/java/myconfig/MyPathConfig.java
+++ demo/src/main/java/myconfig/MyPathConfig.java
@@ -10,10 +10,13 @@ public class MyPathConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		System.out.println("패스컨피그실행");
-		registry.addResourceHandler("/upload/**")
-		//.addResourceLocations("file:///c:/ezwel/upload/");
-		.addResourceLocations("file:////Users/minsu/Documents/ezwel/upload/");
-		
+		if (System.getProperty("os.name").contains("Win")) {
+			registry.addResourceHandler("/upload/**")
+			.addResourceLocations("file:///c:/ezwel/upload/");			
+		} else {
+			registry.addResourceHandler("/upload/**")
+			.addResourceLocations("file:////Users/minsu/Documents/ezwel/upload/");
+		}
 	}
 	
 }
