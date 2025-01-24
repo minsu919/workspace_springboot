fd454264ebdb84058090a2f60cbe84079dfd4a18
minsu <minsur0919@gmail.com>
2025-01-24 10:37:59 +0900

    demo_github spring project 최초 업로드

---
+++ demo_github/src/main/java/boardmapper/BoardMapperService.java
@@ -0,0 +1,66 @@
+package boardmapper;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardMapperService implements BoardService{
+	
+	@Autowired
+	BoardMapper mapper;
+	
+	@Autowired
+	BoardWriterMapper mapper2;
+	
+	@Override
+	public String registerBoard(BoardDTO dto) {
+		int result = mapper.insertBoard(dto);
+		if (result == 1) return "글쓰기성공";
+		else return "글쓰기실패";
+	}
+
+	@Override
+	public List<BoardDTO> pagingList(int pagenum) {
+		int cnt = 3;
+		int start = (pagenum-1)*cnt + 1;
+		int end = pagenum * cnt;
+		int array[] = {start, end};
+		return mapper.pagingList(array);
+	}
+
+	@Override
+	public int totalCount() {
+		return mapper.totalCount();
+	}
+
+	@Override
+	public BoardDTO updateViewcountAndGetDetail(int seq) {
+		mapper.updateViewCount(seq);
+		return mapper.getDetail(seq);
+	}
+
+	@Override
+	public int deleteBoard(int seq) {
+		return mapper.deleteBoard(seq);
+	}
+
+	@Override
+	public int updateBoard(BoardDTO dto) {
+		return mapper.updateBoard(dto);
+	}
+
+	@Override
+	public BoardWriterDTO getBoardWriterInfo(int seq) {
+		return mapper2.getBoardWriterInfo(seq);
+	}
+
+	@Override
+	public MemberBoardDTO getMemberAndboardInfo(String id) {
+		return mapper2.getMemberAndboardInfo(id);
+	}
+	
+	
+
+}
